1c332521b4f0a11e6b6920c03dfd8557e77b9a2e
aap9002 <aap9002@gmail.com>
2023-02-06 23:55:58 +0000

    init and basic react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  return <div class="d-flex app">
+    <h1>Shopping List</h1>
+    <Container/>
     </div>
-  );
+  ;
+}
+
+function Container(){
+  const [items, setList] = useState([])
+  
+  function setNewItem(item_name){
+    if(item_name.trim().length >0)
+      setList([...items,item_name]);
+  }
+
+  return <>
+    <Form handleSubmit={setNewItem}/>
+    <Listitems data={items} />
+    </>
+  ;
+}
+
+function Form(props)
+{
+  const [new_name, set_newname] = useState("");
+
+  function value_changed(e){
+    set_newname(e.target.value);
+  }
+  function submit(e) {
+    props.handleSubmit(new_name);
+    set_newname("");
+    e.preventDefault();
+  }
+
+  return <form onSubmit={submit} class="form w-75">
+    <input type="text" onChange={value_changed} value={new_name} class="form-control"/>
+    <input type="submit" value="Add item" class="btn btn-success w-100 submit-button"/>
+  </form>;
+}
+
+function Listitems(props){
+  const list_items = props.data.map(
+    (val,index)=>
+  <li key={index}>{val}</li>
+  )
+  return <ol>
+    {list_items}
+    </ol>;
 }
 
 export default App;
